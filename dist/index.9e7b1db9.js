4b9a0494a3252a494e7dc759f2c7e1d5db2a8ece
janmejay0312 <janmejay351@gmail.com>
2022-12-29 13:05:44 +0530

    first commit

---
+++ dist/index.9e7b1db9.js
@@ -0,0 +1,9 @@
+const heading = React.createElement("h1", {
+    id: "hl"
+}, "Hello World");
+console.log(heading);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+console.log(root);
+root.render(heading);
+
+//# sourceMappingURL=index.9e7b1db9.js.map
